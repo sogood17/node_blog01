e0a07f78d1950a3965b737915172e7cfdf5acdeb
Gayoung Kim <sogoodpiano@gmail.com>
2025-01-02 22:55:56 +0900

    back router init

--- server/server.js
+++ server/server.js
@@ -8,10 +8,17 @@ app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
 
-app.get('/test', (req, res) => {
-  res.send('test');
-  console.log(req.body);
+app.get('/', (req, res) => {
+  res.send('Hello!');
 })
+
+// app.get('/category', (req, res) => {
+//   res.send('category');
+// })
+
+// app.get('/login', (req, res) => {
+//   res.send('login')
+// })
 
 app.listen(port, () => {
   console.log("Server is running at "+port);
