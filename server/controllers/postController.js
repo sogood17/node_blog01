7959c42be31e88100f737b4f2dc5b22272437552
Gayoung Kim <sogoodpiano@gmail.com>
2025-01-16 19:12:30 +0900

    update router, get all posts api, create a post api

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -1,5 +1,21 @@
-const Post = require('../models/Post');
+const postService = require("../services/postController");
 
+// get all posts
 exports.getAllPosts = async (req, res) => {
+  try {
+    const posts = await postService.getAllPosts();
+    res.status(200).json(posts);
+  } catch (err) {
+    res.status(500).json( {message : err.message});
+  }
+}
 
+//create post
+exports.createPost = async (req, res) => {
+  try {
+    const post = await postService.createPost();
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(500).json({message : err.message});
+  }
 }
--- server/routes/main.js
+++ server/routes/main.js
@@ -1,2 +1,10 @@
 const express = require("express");
 const router = express.Router();
+
+router.get('/', getAllPosts);
+router.post('/', createPost);
+router.get('/:id', getPostById);
+router.put('/:id', updatePostById);
+router.delete('/:id', deleteUserById);
+
+module.exports= router;
