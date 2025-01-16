ad08ae46e2c682220c389d62a10a20d56ceef602
Gayoung Kim <sogoodpiano@gmail.com>
2025-01-16 20:05:59 +0900

    make update post api, delete post api

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -13,7 +13,7 @@ exports.getAllPosts = async (req, res) => {
 //create post
 exports.createPost = async (req, res) => {
   try {
-    const post = await postService.createPost();
+    const post = await postService.createPost(req.body);
     res.status(200).json(post);
   } catch (err) {
     res.status(500).json({message : err.message});
--- server/services/postService.js
+++ server/services/postService.js
@@ -10,3 +10,18 @@ exports.createPost = async (postData) => {
   const newPost = new Post(postData);
   return await newPost.save();
 };
+
+// get post by id
+exports.getPost = async (id) => {
+  return await Post.findById(id);
+}
+
+// update post by id
+exports.updatePost = async (id, updatedPostData) => {
+  const updatedPost = Post.findByIdAndUpdate(id, updatedPostData, { new: true, runValidators: true })
+}
+
+// delete post by id
+exports.deletePost = async (id) => {
+  const deletedPost = Post.findByIdAndDelete(id);
+}
